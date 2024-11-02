5fce6f45d3a4273cc7b7bcb2b2d80bc8ad2b2f51
ManoGlicose <37117958+ManoGlicose@users.noreply.github.com>
2024-11-02 17:44:06 -0300

    Fighting animations
    Animações de combate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,10 +19,18 @@ public class PlayerController : MonoBehaviour
     public float turnSmoothTime = 0.25f;
     float turnSmoothVelocity;
 
+    // Attacks (my own system)
+    public int numberOfClicks = 0;
+    float resetComboTime = 0.5f;
+
+    public float attackTimer = 0;
+
     private void Awake()
     {
         controls = new PlayerActions();
         mainCamera = Camera.main;
+
+        controls.Controls.Attack.performed += ctx => CountAttacks();
     }
 
     // Start is called before the first frame update
@@ -41,19 +49,31 @@ public class PlayerController : MonoBehaviour
 
         if (direction.magnitude > 0.1f)
         {
+            if (numberOfClicks <= 0)
+            {
+                float actualSpeed = inputDirection.magnitude > 0.6f ? speed : speed / 2;
 
-            float actualSpeed = inputDirection.magnitude > 0.6f ? speed : speed / 2;
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCamera.transform.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0, angle, 0);
 
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCamera.transform.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0, angle, 0);
+                Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
 
-            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
-
-            controller.Move(moveDirection.normalized * actualSpeed * Time.deltaTime);
+                controller.Move(moveDirection.normalized * actualSpeed * Time.deltaTime);
+            }
         }
 
         #endregion
+
+        if (attackTimer > 0)
+        {
+            attackTimer -= Time.deltaTime;
+        }
+        else 
+        { 
+            attackTimer = 0;
+            numberOfClicks = 0;
+        }
 
         HandleAnimations();
     }
@@ -62,8 +82,69 @@ public class PlayerController : MonoBehaviour
     {
         //print(inputDirection.magnitude);
         animator.SetFloat("Speed", inputDirection.magnitude);
+
+        //if (numberOfClicks >= 3 && attackTimer > 0) return;
+
+        switch (numberOfClicks)
+        {
+            case 0:
+                animator.SetBool("Hit1", false);
+                animator.SetBool("Hit2", false);
+                animator.SetBool("Hit3", false);
+                break;
+            case 1:
+                animator.SetBool("Hit1", true);
+                break;
+            case 2:
+                animator.SetBool("Hit2", true);
+                break;
+            case 3:
+                animator.SetBool("Hit3", true);
+                break;
+            default:
+                animator.SetBool("Hit1", false);
+                animator.SetBool("Hit2", false);
+                animator.SetBool("Hit3", false);
+                break;
+        }
+
+        //animator.SetInteger("Hits", numberOfClicks);
     }
 
+    void CountAttacks()
+    {
+        if(numberOfClicks < 3)
+            numberOfClicks++;
+        //float newWaitTimer = 0;
+
+        //for (int i = 0; i < numberOfClicks; i++)
+        //{
+        //    switch (i)
+        //    {
+        //        case 1:
+        //            newWaitTimer = 0.2f;
+        //            break;
+        //        case 2:
+        //            newWaitTimer = 0.27f;
+        //            break;
+        //        case 3:
+        //            newWaitTimer = 0.35f;
+        //            break;
+        //        default:
+        //            break;
+        //    }
+        //}
+
+        SetAttackTimer(numberOfClicks < 3 ? 0.65f - (0.65f * 0.25f) : 0.9f);
+        //print("Attack n. " + numberOfClicks.ToString() + " performed");
+    }
+
+    void SetAttackTimer(float newTime)
+    {
+        attackTimer = newTime;
+    }
+
+    #region Enable Disable Input System
     private void OnEnable()
     {
         controls.Enable();
@@ -73,4 +154,5 @@ public class PlayerController : MonoBehaviour
     {
         controls.Disable();
     }
+    #endregion
 }
